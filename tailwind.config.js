9862e0da815513192d301384be3a5cdc83e1c7ce
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-06-15 22:04:12 +0200

    add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,28 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontSize: {
-      'headerSize': ['clamp(2rem, 10vw, 8rem)',{
-        fontWeight: '900'
-      }],
-      "iconSize": "1.5rem"
+      headerSize: [
+        "clamp(2rem, 10vw, 8rem)",
+        {
+          fontWeight: "900",
+        },
+      ],
+      iconSize: "1.5rem",
+      timeSize: [
+        "clamp(1rem, 10vw, 2rem)",
+        {
+          fontWeight: "900",
+        },
+      ],
     },
     extend: {
       colors: {
         mainFontColor: "#fff",
-        formButtonColor: "bg-orange-500"
-      }
+        formButtonColor: "bg-orange-500",
+      },
+      spacing: {
+        "inputErrorPosition": "-25px",
+      },
     },
   },
   plugins: [],
