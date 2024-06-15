69ca135265f96fa8cc715659d29069f5e0abb364
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-06-15 22:03:31 +0200

    set locaLStorage htmlElement

--- src/script/index.js
+++ src/script/index.js
@@ -1,14 +1,16 @@
 import { CreateElement } from "./CreateElement.js";
 import { UpdateTaskDoneTitle } from "./UpdateTaskDoneTitle.js";
+import DateOnWebsite from "./DateOnWebsite.js";
 
 const todo = (function () {
   const inputValue = document.querySelector("#input_value");
   const setInputButton = document.querySelector("#set_input_button");
   const taskDone = document.querySelector("#task-done");
-  
-
+  const inputError = document.querySelector("#inpurError");
 
   let inputResult = "";
+  let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+  const todoListTask = [];
   const doneTodoList = [];
   const { addElement, appendElement } = CreateElement();
 
@@ -18,13 +20,22 @@ const todo = (function () {
 
   setInputButton.addEventListener("click", (e) => {
     e.preventDefault();
-    if (inputResult.trim() === "") return;
-
     const elements = addElement();
+    if (!/^[a-zA-Z0-9]*$/g.test(inputValue.value)) {
+      inputResult = "";
+      inputValue.value = "";
+      inputError.textContent = "A task can contain only letters and numbers";
+    } else {
+      inputError.textContent = "";
+      // localStorage.setItem("taskList", JSON.stringify(todoListTask));
+    }
+    if (inputResult.trim() === "") return;
 
     elements.newLi.innerHTML = inputResult;
     appendElement(elements);
-    
+    taskList.push(elements.newLi.outerHTML);
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+
     elements.treshButton.addEventListener("click", () => {
       const index = doneTodoList.indexOf(elements.newLi);
       if (index !== -1) {
@@ -49,4 +60,15 @@ const todo = (function () {
     inputResult = "";
     inputValue.value = "";
   });
+  taskList.map((item, key) => {
+    const elements = addElement();
+    elements.newLi.innerHTML = item;
+    return appendElement(elements);
+  });
+
+  if (!localStorage.getItem("taskList")) {
+    localStorage.setItem("taskList", JSON.stringify([]));
+  }
+
+  DateOnWebsite();
 })();
